d79e9220dbe54d7dc4da4a6787085b5a6182f885
HackedSoul <44831870+HackedS0ul@users.noreply.github.com>
2022-12-22 03:38:23 +0200

    bug fixes

--- extractData/brute.py
+++ extractData/brute.py
@@ -1,41 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
-import os 
-from os.path import exists
 
 target_url = "https://esaurida.lt"
 
 resp = requests.get(target_url)
 file = 'data.txt'
 soup = BeautifulSoup(resp.content, 'html.parser')
-links = []
+temp_links = []
 # returns title of the page
 #print(soup.title.string)
 
 # get and add links into list 
 getLinks = soup.find_all('a', href=True)
-# checking that any link is available 
+# checking that any link is available
+links = []
 if getLinks:
     for i in getLinks:
-        
-        if i is not links:
-            links.append(i['href'].strip())
+
+        if i not in temp_links:
+            temp_links.append(i['href'].strip())
+            print(len(temp_links))
         else:
             print("record is existing!")
     
-
 else:
     print("Can't find any link")
 #length of links
 #print(len(links))
 
+# removing duplicates for data file
 
+for data in temp_links:
+    if data not in links:
+        links.append(data)
+    else:
+        print("Url found in data file.. ")
+
+        print("removing element on line " + str(len(links)))
 
 
 with open(file, 'w+', encoding='utf-8') as f:
-    if result is not file:
-       f.write(str("\n".join(links)))
-    else:
-        os.remove()
-        
-
+    f.write(str("\n".join(links)))
